ee9f9459228d6597d7d5416120f33af87e33d18a
Shintaro Nohira <nohira@crebo.co.jp>
2026-02-05 17:01:12 +0900

    保存ボタンで入力内容をalert表示

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,26 +1,48 @@
+import { useState } from "react";//入力内容を管理する
+
 type Props = {
   onBack: () => void;
 };//TypeScriptでReactコンポーネントのprops（受け取る引数）の型を定義している
 
 export default function Record({ onBack }: Props) {
+  const [exercise, setExercise] = useState("");//種目（文字）
+  const [time, setTime] = useState(0);//時間（数字）
+
   return (
     <div>
       <h2>記録ページ</h2>
 
       <div>
         <label>種目:</label>
-        <input type="text"/>
+        <input 
+          type="text" 
+          value={exercise}
+          onChange={(e) => setExercise(e.target.value)}
+        />
       </div>
 
       <div>
         <label>時間:</label>
-        <input type="number"/>
+        <input 
+          type="number"
+          value={time}  
+          onChange={(e) => setTime(Number(e.target.value))}//value と onChange はセット
+        />
       </div>
 
-      <button>保存</button>
+      <button
+        onClick={() => {
+          alert(`種目: ${exercise} / 時間: ${time}`);
+        }}
+          >保存</button>
 
       <button onClick={onBack}>戻る</button>
+        
+       <p>種目: {exercise}</p>
+       <p>時間: {time}</p>
+
     </div>
+
   );
 }
  
